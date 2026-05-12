5cc8f69830e5904bb947590fd3afeb5e7b314ac7
The-Rildot <ronytsirkel@gmail.com>
2026-05-12 12:49:00 -0400

    Updated Robot Character

--- src/components/character/parts/blush/RobotBlushPart.tsx
+++ src/components/character/parts/blush/RobotBlushPart.tsx
@@ -1,8 +1,24 @@
 export function RobotBlushPart() {
   return (
     <div className="part-blush part-blush--robot">
-      <span />
-      <span />
+      <svg
+        className="part-blush__zigzag part-blush__zigzag--left"
+        viewBox="0 0 24 8"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <polyline points="0,4 4,1 8,7 12,1 16,7 20,1 24,4" />
+      </svg>
+      <svg
+        className="part-blush__zigzag part-blush__zigzag--right"
+        viewBox="0 0 24 8"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <polyline points="0,4 4,1 8,7 12,1 16,7 20,1 24,4" />
+      </svg>
     </div>
   );
 }
